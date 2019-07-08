c251add4b8068510b27801e1b30209310ec0dac1
setiawanswan <setiawan.tumilin@gmail.com>
2019-07-08 16:09:54 +0700

    learn about JSX

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+const name = "my name is wawan";
+const element = <h1> Hallo {name} !! </h1>;
+ReactDOM.render(element, document.getElementById('master'))
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
