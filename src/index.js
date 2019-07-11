4276f8fb21d85aefafeabd200a4c77c2b9dca952
setiawanswan <setiawan.tumilin@gmail.com>
2019-07-11 15:51:54 +0700

    parse data from parent to child

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import faker from 'faker';
+import faker from 'faker';
 import CommentDetails from './CommentDetails'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetails />
-            <CommentDetails />
-            <CommentDetails />
-            <CommentDetails />
-            <CommentDetails />
-            <CommentDetails />
-            <CommentDetails />
-            <CommentDetails />
+            <CommentDetails 
+                author="Sam" 
+                timeAgo="Today at 4:45PM" 
+                content="Nice blog post!" 
+                avatar = {faker.image.avatar()}
+            />
+            <CommentDetails 
+                author="Alex" 
+                timeAgo="Today at 2:00AM" 
+                content="I like this subject" 
+                avatar = {faker.image.avatar()}
+            />
+            <CommentDetails 
+                author="Jane" 
+                timeAgo="Today at 3:16PM" 
+                content="I love you" 
+                avatar = {faker.image.avatar()}
+            />
+            <CommentDetails 
+                author="John" 
+                timeAgo="Today at 1:30PM" 
+                content="I like play footbal" 
+                avatar = {faker.image.avatar()}
+            />
         </div>
     );
 };
