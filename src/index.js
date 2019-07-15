4a44c8c927542a856a1b4db13092dee1adb031e0
setiawanswan <setiawan.tumilin@gmail.com>
2019-07-15 14:34:10 +0700

    add table approve

--- src/index.js
+++ src/index.js
@@ -2,34 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetails from './CommentDetails'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetails 
-                author="Sam" 
-                timeAgo="Today at 4:45PM" 
-                content="Nice blog post!" 
-                avatar = {faker.image.avatar()}
-            />
-            <CommentDetails 
-                author="Alex" 
-                timeAgo="Today at 2:00AM" 
-                content="I like this subject" 
-                avatar = {faker.image.avatar()}
-            />
-            <CommentDetails 
-                author="Jane" 
-                timeAgo="Today at 3:16PM" 
-                content="I love you" 
-                avatar = {faker.image.avatar()}
-            />
-            <CommentDetails 
-                author="John" 
-                timeAgo="Today at 1:30PM" 
-                content="I like play footbal" 
-                avatar = {faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning !</h4>
+                    Are you sure want to do this?
+                </div>    
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetails 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    content="Nice blog post!" 
+                    avatar = {faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetails 
+                    author="Alex" 
+                    timeAgo="Today at 2:00AM" 
+                    content="I like this subject" 
+                    avatar = {faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetails 
+                    author="Jane" 
+                    timeAgo="Today at 3:16PM" 
+                    content="I love you" 
+                    avatar = {faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetails 
+                    author="John" 
+                    timeAgo="Today at 1:30PM" 
+                    content="I like play footbal" 
+                    avatar = {faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
